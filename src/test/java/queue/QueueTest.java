cce883b024aebeada45ef547c8d284891b7a3970
Ryan Vinnícius <ryan.vinnicius.edu@gmail.com>
2025-02-13 20:28:24 -0300

    .

---
+++ src/test/java/queue/QueueTest.java
@@ -0,0 +1,16 @@
+package queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class QueueTest {
+
+	
+
+}
