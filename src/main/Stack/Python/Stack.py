02c85a0ad1394c3880415b3c80d541ca9628e726
matheusleitebarbosasouza <matheus.leite.barbosa.souza@ccc.ufcg.edu.br>
2025-02-20 15:15:55 -0300

    Update Stack.py

--- src/main/Stack/Python/Stack.py
+++ src/main/Stack/Python/Stack.py
@@ -1,10 +1,13 @@
 
 class Stack: 
    tail = -1 
+   array= [0]
    def __init__(self, capacidade):
-        self.capacidade = capacidade
+        self.array = [0 for x in range capacidadd]
    def pop():
-       return False
+       V= array[tail]
+       tail--
+       return V
    def isEmpty():
        if(self.tail<0):
            return True
@@ -15,7 +18,9 @@ class Stack:
        if(tail+1==self.capacidade.length):
            return True
        return False
-
+   def push(valor):
+      array[++tail] = valor
+   def removeFirst()
     def peek():
         return False
     def size():
@@ -26,5 +31,3 @@ class Stack:
 class main:
    stack1 = Stack(9)
    print(stack1.capacidade)
-    
-    
