3bd8ce69fb29c93de1d54825b0a902517e85e13e
matheusleitebarbosasouza <matheus.leite.barbosa.souza@ccc.ufcg.edu.br>
2025-02-20 09:38:16 -0300

    Update Stack.py

--- src/main/Stack/Python/Stack.java
+++ src/main/Stack/Python/Stack.java
@@ -1,1 +1,30 @@
 
+class Stack: 
+   tail = -1 
+   def __init__(self, capacidade):
+        self.capacidade = capacidade
+   def pop():
+       return False
+   def isEmpty():
+       if(self.tail<0):
+           return True
+       return False
+   def toString():
+       return False
+   def isFull():
+       if(tail+1==self.capacidade.length):
+           return True
+       return False
+
+    def peek():
+        return False
+    def size():
+        return self.tail+1
+
+   def indexOf():
+       return False
+class main:
+   stack1 = Stack(9)
+   print(stack1.capacidade)
+    
+    
