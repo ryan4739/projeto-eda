b70685e77a1ff2679f2c5255fd11213ce95bb3d9
matheusleitebarbosasouza <matheus.leite.barbosa.souza@ccc.ufcg.edu.br>
2025-02-20 15:34:21 -0300

    Update Stack.py

--- src/main/Stack/Python/Stack.py
+++ src/main/Stack/Python/Stack.py
@@ -13,16 +13,35 @@ class Stack:
            return True
        return False
    def toString():
-       return False
+       stregyro= ""
+      for i in range(array.length):
+         stregyro+= str(array[i])
+         if(!isEmpty()):
+            Stregyro+= ", "
+            tail--
+         return stregyro
    def isFull():
        if(tail+1==self.capacidade.length):
            return True
        return False
    def push(valor):
       array[++tail] = valor
-   def removeFirst()
-    def peek():
-        return False
+   def esquerda(valor):
+      for i in range(valor,array.length):
+         array[i-1] = array[i]
+   def esquerda(valor):
+      for i in range(valor,array.length):
+         array[i-1] = array[i]
+   def removeLast():
+      esquerda(1)
+      tail--
+   def removeIndex(index):
+      esquerda (index)
+      tail--
+   def pushIndex(index):
+      
+    def peek(index):
+        return array[index]
     def size():
         return self.tail+1
 
