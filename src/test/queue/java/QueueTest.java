b57839f67aa3973baaec95d5168b5ff88f978f4e
Ryan Vinnícius <ryan.vinnicius.edu@gmail.com>
2025-02-21 06:48:25 -0300

    Pequenas alterações

--- src/test/queue/java/QueueTest.java
+++ src/test/queue/java/QueueTest.java
@@ -10,7 +10,19 @@ import org.junit.jupiter.api.Test;
 
 
 public class QueueTest {
-
 	
+	private Queue seq_1;
+	private Queue seq_2;
+
+	@BeforeEach
+	void prepareTest() {
+		seq_1 = new Queue(10);
+		seq_2 = new Queue(0);
+	}
+
+	@Test
+	void insertElementIntoAFullQueue() {
+			
+	} 
 
 }
