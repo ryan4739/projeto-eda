3fd0278c2d99fdfb6a1c5fb193ee8c3b60a7394c
Ryan Vinnícius <ryan.vinnicius.edu@gmail.com>
2025-03-02 23:55:30 -0300

    Estruturação dos métodos

---
+++ src/main/queue/python/Queue.py
@@ -0,0 +1,43 @@
+class Queue:
+
+    def __init__(self, capacity):
+        #TODO
+
+    def is_empty(self):
+        #TODO
+
+    def is_full(self):
+        #TODO
+
+    def add_last(self, element):
+        #TODO
+
+    def add_first(self, element):
+        #TODO
+
+    def add(self, element, index):
+        #TODO
+
+    def remove_first(self):
+        #TODO
+
+    def remove_last(self):
+        #TODO
+
+    def remove(self, index):
+        #TODO
+
+    def get_first(self):
+        #TODO
+
+    def get_last(self):
+        #TODO
+
+    def get(self, index):
+        #TODO
+
+    def index_of(self, element):
+        #TODO
+
+    def last_index_of(self, element):
+        #TODO
---
+++ src/test/queue/python/QueueTest.py
@@ -0,0 +1,6 @@
+import unittest
+
+class QueueTest(unittest.TestCase):
+
+    def add_last_with_space(self):
+        #TODO
