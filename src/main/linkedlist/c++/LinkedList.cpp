fe96dad3300419d10642308a70c1f016b3dd9b2f
Arthur Correia <arthurcorreialeite@gmail.com>
2025-03-03 20:49:36 -0300

    refactor: Métodos e estrutura de linkelist-c++

--- src/main/linkedlist/c++/LinkedList.cpp
+++ src/main/linkedlist/c++/LinkedList.cpp
@@ -1,97 +1,223 @@
 #include <iostream>
+#include <stdexcept>
+#include <string> 
+
 using namespace std;
 
 struct Node {
     int data;
     Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
 };
 
 class LinkedList {
     Node* head;
+    Node* tail;
+    int size;
 
-    public:
-        LinkedList() : head(NULL) {}
+    Node* getNodeByIndex(int index) {
+        if (index < 0 || index >= this->size) {
+            throw std::out_of_range("Índice inválido");
+        }
 
-        void insereNoInicio(int valor) {
-            Node* novoNode = new Node();
-            novoNode->data = valor;
-            novoNode->next = head;
+        Node* node = head;
+        for (int i = 0; i < index; i++) {
+            node = node->next;
+        }
+        return node;
+    }
+
+public:
+
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    void addFirst(int value) {
+        Node* newNode = new Node(value);
+        
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode->next = head;
             head = newNode;
         }
-
-        void insereNoIndice(int valor, int indice) {
-            if (posicao < 1) {
-                cout << "Índice inválido!" << endl;
-                return;
-            }
-        }
-
-        void insereNoFim(int valor) {
-            Node* novoNode = new Node();
-            novoNode->data = valor;
-            novoNode->next = NULL;
-
-            if (!head) {
-                head = novoNode;
-                return;
-            }
-
-            Node* nodeAtual = head;
-            while (nodeAtual->next) {
-                temp = temp->next;
-            }
-
-            temp->next = novoNode;
-        }
-
-        void deletaPrimeiroNode() {
-            if (!head) { return; }
-
-            Node* nodeAtual = head;
-            head = head->next;
-            delete nodeAtual;
-        }
-
-        void deletaPorIndice(int indice) {
-            if (indice == 0) {
-                deletaPrimeiroNode();
-                return;
-            }
-
-            Node* nodeAtual = head;
-            int posicao = 0;
-            while (posicao < indice - 1 && temp) {
-                temp = temp->next;
-                ++posicao;
-            }
-            
-            if (!nodeAtual || !nodeAtual->next) {
-                cout << "Índice inválido!" << endl;
-                return;
-            }
-
-            Node* nodeParaApagar = nodeAtual->next;
-            nodeAtual->next = temp->next->next;
-            delete nodeParaApagar;
-        }
-
-        void deletaUltimoNode() {
-            if (!head) { return; }
-
-            if (!head->next) {
-                deletaPrimeiroNode();
-                return;
-            }
-
-            Node* nodeAtual = head;
-            while (nodeAtual->next->next) {
-                nodeAtual = nodeAtual->next;
-            }
-
-            delete nodeAtual->next;
-            nodeAtual->next = NULL;
-
-        }
-
+        size++;
+    }
+    
+    void addLast(int value) {
+        Node* newNode = new Node(value);
         
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    void add(int value, int index) {
+        if (index < 0 || index > size) {
+            throw std::out_of_range("Índice inválido");
+        }
+        
+        if (index == 0 || isEmpty()) {
+            addFirst(value);
+        } else if (index == size) {
+            addLast(value);
+        } else {
+            Node* newNode = new Node(value);
+            Node* prevNode = getNodeByIndex(index-1);
+            Node* aftNode = prevNode->next;
+
+            prevNode->next = newNode;
+            newNode->next = aftNode;
+
+            size++;
+        }
+    }
+
+    int getFirst() {
+        return head->data;
+    }
+
+    int getLast() {
+        return tail->data;
+    }
+
+    int get(int index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("índice inválido");
+        }
+
+        Node* node = getNodeByIndex(index);
+        return node->data;
+    }
+
+    void updateNode(int index, int value) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("índice inválido");
+        }
+
+        Node* node = getNodeByIndex(index);
+        node->data = value;
+    }
+    
+    void removeFirst() {
+        if (isEmpty()) {
+            throw std::out_of_range("Lista vazia");
+        }
+
+        if (size == 1) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            Node* currNode = head;
+            head = head->next;
+            delete currNode;
+        }
+        size--;
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            throw std::out_of_range("Lista vazia");
+        }
+
+        if (size == 1) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            Node* prevNode = getNodeByIndex(size-2);
+            delete prevNode->next;
+            prevNode->next = nullptr;
+            tail = prevNode;
+        }
+        size--;
+    }
+
+    void removeByIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw std::out_of_range("Índice inválido");
+        }
+
+        if (index == 0) {
+            return removeFirst();
+        } else if (index == size-1) {
+            return removeLast();
+        }
+
+        Node* prevNode = getNodeByIndex(index-1);
+        Node* toBeRemoved = prevNode->next;
+
+        prevNode->next = prevNode->next->next;
+        delete toBeRemoved;
+
+        size--;
+    }
+
+    void removeByValue(int value) {
+        int index = indexOf(value);
+        if (index != -1) {
+            removeByIndex(index);
+        }
+    }
+
+    int indexOf(int value) {
+        if (isEmpty()) {
+            throw std::exception("Lista vazia");
+        }
+
+        Node* node = head;
+        for (int i = 0; i < size; i++) {
+            if (node->data == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int lastIndexOf(int value) {
+        if (isEmpty()) {
+            throw std::exception("Lista vazia");
+        }
+
+        int index = -1;
+
+        Node* node = head;
+        for (int i = 0; i < size; i++) {
+            if (node->data == value) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    int sizeLL() {
+        return size;
+    }
+
+    bool contains(int value) {
+        return indexOf(value) != -1;
+    }
+
+    string toString() {
+        string llString = "";
+
+        Node* node = head;
+        while (node != nullptr) {
+            llString += to_string(node->data);
+            if (node->next != nullptr) {
+                llString += " -> ";
+            }
+            node = node->next;
+        }
+        return llString;
+    } 
 };
