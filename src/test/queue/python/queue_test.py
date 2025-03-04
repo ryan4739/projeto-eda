5af0b84149e57d8a01082b1aebacda6451e5fc98
Ryan Vinnícius <ryan.vinnicius.edu@gmail.com>
2025-03-04 09:44:08 -0300

    Complementos ao teste de add_last

--- src/test/queue/python/queue_test.py
+++ src/test/queue/python/queue_test.py
@@ -10,5 +10,18 @@ class QueueTest(unittest.TestCase):
         self.assertTrue(q.is_empty())
         self.assertFalse(q.is_full())
 
+    def test_add_last(self):
+        q = Queue(3)
+        q.add_last(10)
+        self.assertEqual(q.get_last(), 10)
+        q.add_last(20)
+        self.assertEqual(q.get_last(), 20)
+        q.add_last(30)
+        self.assertEqual(q.get_last(), 30)
+        self.assertEqual(q.size, 3)
+        self.assertTrue(q.is_full())
+        with self.assertRaises(Exception): q.add_last(40)
+        self.assertEqual(q.get_last(), 30)
+
 if __name__ == '__main__':
     unittest.main()
