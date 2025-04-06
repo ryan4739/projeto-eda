4294b599de68485bc8a518040d03571065418cdf
Diogo Medeiros <140714566+diogo-med@users.noreply.github.com>
2025-04-06 16:22:17 -0300

    .

--- src/main/deque/python/ExecTime.py
+++ src/main/deque/python/ExecTime.py
@@ -8,29 +8,28 @@ def measure_time(func_name, input_list):
     n = len(input_list)
     results = []
     dq = Deque(n)
-    for num in input_list:
-        dq.add_last(num)
+    
     for _ in range(30):
-            func = getattr(dq, func_name) 
-            if not callable(func):
-                raise ValueError(f"'{func_name}' não é uma função válida do objeto.")
-            else:
-                if func_name.endswith("last") or func_name.endswith("first"):
-                    if func_name.startswith("add"):
-                        start = time.perf_counter_ns()
-                        func(10)
-                        end = time.perf_counter_ns()
-
-                    else:
-                        start = time.perf_counter_ns()
-                        func()
-                        end = time.perf_counter_ns()
+        for num in input_list:
+            dq.add_last(num)
+        func = getattr(dq, func_name) 
+        if not callable(func):
+            raise ValueError(f"'{func_name}' não é uma função válida do objeto.")
+        else:
+            if func_name.endswith("last") or func_name.endswith("first"):
+                if func_name.startswith("add"):
+                    start = time.perf_counter_ns()
+                    func(10)
+                    end = time.perf_counter_ns()
                 else:
                     start = time.perf_counter_ns()
-                    func(10, n//2)
+                    func()
                     end = time.perf_counter_ns()
-
-                results.append(end - start)
+            else:
+                start = time.perf_counter_ns()
+                func(10, n//2)
+                end = time.perf_counter_ns()
+            results.append(end - start)
     results = sorted(results)
     output_line = f"deque-python {results[14]} {n}"
     
