fa0019dad4f146d67c1bae29b45a16fcb3aad267
Diogo Medeiros <140714566+diogo-med@users.noreply.github.com>
2025-04-07 17:25:02 -0300

    Update ExecMEMO.py

--- src/main/deque/python/ExecMEMO.py
+++ src/main/deque/python/ExecMEMO.py
@@ -6,34 +6,43 @@ from deque import Deque
 
 def measure_memory(func_name, input_list):
     n = len(input_list)
-    dq = Deque(n)
+    dq = Deque(n+1)
     for num in input_list:
         dq.add_last(num)
-
+    
     func = getattr(dq, func_name)
 
     if not callable(func):
         raise ValueError(f"'{func_name}' não é uma função válida do objeto.")
     else:
         tracemalloc.start()
+        snapshot1 = tracemalloc.take_snapshot()
+
+        # Executa o método
         if func_name.endswith("last") or func_name.endswith("first"):
             if func_name.startswith("add"):
                 func(10)
             else:
                 func()
         else:
-            func(10,n//2)
-        current, peak_memory = tracemalloc.get_traced_memory()
+            func(10, n//2)
+
+        snapshot2 = tracemalloc.take_snapshot()
         tracemalloc.stop()
 
-    output_line = f"deque-python {peak_memory}(bytes) {n}"
+        stats = snapshot2.compare_to(snapshot1, 'lineno')
+        peak_memory = sum([stat.size_diff for stat in stats])
+    
+    output_line = f"deque-python {peak_memory} {n}"
 
-    file_name = f"{func_name}MEMO.data"
+    output_dir = "data/results/memory"
+    os.makedirs(output_dir, exist_ok=True)  # Garante que a pasta exista
+    file_name = os.path.join(output_dir, f"{func_name}MEMO.data")
     is_new_file = not os.path.exists(file_name) or os.stat(file_name).st_size == 0
 
     with open(file_name, "a") as file:
         if is_new_file:
-            file.write("estrutura-linguagem pico_da_memoria tamanho_da_entrada\n")
+            file.write("estrutura_linguagem memoria tamanho\n")
         file.write(output_line + "\n")
 
 methods = ["add_first", "add_last", 
@@ -46,5 +55,6 @@ for line in sys.stdin:
         continue
 
     input_list = list(map(int, line.split()))
+    
     for method in methods:
         measure_memory(method, input_list)
