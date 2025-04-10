b5597d84947f08b8822bc19de9e9b6aeaa994957
Ryan Vinnícius <ryan.vinnicius.edu@gmail.com>
2025-04-10 14:31:49 -0300

    Finalização da interação

--- scripts/R/plotTime.R
+++ scripts/R/plotTime.R
@@ -7,19 +7,15 @@ output_file <- paste0("data/results/time/", args[1], ".html")
 
 data = read.table(input_file, header = TRUE)
 
-p <- ggplot(data, aes(x = tamanho, y = tempo, colour = estrutura_linguagem)) +
+p <- ggplot(data, aes(x = tamanho, y = memoria, colour = estrutura_linguagem)) +
   geom_line() +
-  labs(title = paste0("Tempo de execução por tamanho para método ", args[1]),
+  geom_point() +
+  labs(title = paste0("Tempo de execução por memória para método ", args[1]),
        x = "Tamanho (número de inteiros por linha)",
        y = "Tempo (ns)",
 		   colour = "Estrutura/linguagem") +
   theme_bw() +
   theme(legend.position = "bottom")
 
-ggsave(output_file,
-       plot = p,
-       width = 210,
-       height = 148,
-       dpi = 300,
-       units = "mm")
-
+interactive_plot <- ggplotly(p)
+htmlwidgets::saveWidget(interactive_plot, file = output_file, selfcontained = TRUE)
