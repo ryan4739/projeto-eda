a51e33469fe775a5faf66e4fb9f7d0f5618987f1
Ryan Vinnícius Tielles Almeida da Silva <ryan.vinnicius.tielles.almeida.silva@ccc.ufcg.edu.br>
2025-04-10 14:22:41 -0300

    .

--- scripts/R/plotMemory.R
+++ scripts/R/plotMemory.R
@@ -1,24 +1,22 @@
 library('ggplot2')
+library('plotly')
 
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- paste0("data/results/memory/", args[1], ".data")
-output_file <- paste0("data/results/memory/", args[1], ".png")
+output_file <- paste0("data/results/memory/", args[1], ".html")
 
 data = read.table(input_file, header = TRUE)
 
 p <- ggplot(data, aes(x = tamanho, y = memoria, colour = estrutura_linguagem)) +
   geom_line() +
+  geom_point() +
   labs(title = paste0("Tempo de execução por memória para método ", args[1]),
-       x = "Tamanho",
-       y = "Memória",
+       x = "Tamanho (número de inteiros por linha)",
+       y = "Memória (kB)",
 		   colour = "Estrutura/linguagem") +
   theme_bw() +
   theme(legend.position = "bottom")
 
-ggsave(output_file,
-       plot = p,
-       width = 210,
-       height = 148,
-       dpi = 300,
-       units = "mm")
+interactive_plot <- ggplotly(p)
+htmlwidgets::saveWidget(interactive_plot, file = output_file, selfcontained = TRUE)
 
--- scripts/R/plotTime.R
+++ scripts/R/plotTime.R
@@ -1,15 +1,16 @@
 library('ggplot2')
+library('plotly')
 
 args <- commandArgs(trailingOnly = TRUE)
 input_file <- paste0("data/results/time/", args[1], ".data")
-output_file <- paste0("data/results/time/", args[1], ".png")
+output_file <- paste0("data/results/time/", args[1], ".html")
 
 data = read.table(input_file, header = TRUE)
 
 p <- ggplot(data, aes(x = tamanho, y = tempo, colour = estrutura_linguagem)) +
   geom_line() +
   labs(title = paste0("Tempo de execução por tamanho para método ", args[1]),
-       x = "Tamanho",
+       x = "Tamanho (número de inteiros por linha)",
        y = "Tempo (ns)",
 		   colour = "Estrutura/linguagem") +
   theme_bw() +
